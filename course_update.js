46a7269389bdbb3cd1b54345603f85ca2191bea5
Kaluchii <mr.kaluchii@mail.ru>
2018-02-16 23:18:02 +0600

    Add check result

--- course_update.js
+++ course_update.js
@@ -14,7 +14,7 @@ page.open('https://www.bcc.kz/about/kursy-valyut/', function(status) {
         setTimeout(function () {
             var courses = page.evaluate(function() {
                 var courseObj = { courses: {} };
-                courseObj.courses.usd = $('.bcc_fulla .s_table_over:nth-child(5) tbody tr:nth-child(3) td:nth-child(3)').text();
+                courseObj.courses.usd = $('.bcc_full .s_table_over:nth-child(5) tbody tr:nth-child(3) td:nth-child(3)').text();
                 console.log(courseObj.courses.usd);
                 return courseObj;
             }, 'courses');
