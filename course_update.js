00855f087db6c0bb32352d8801a04ac905610239
Kaluchii <mr.kaluchii@mail.ru>
2018-02-16 23:13:13 +0600

    Add check result

--- course_update.js
+++ course_update.js
@@ -2,7 +2,11 @@ var page = require('webpage').create();
 var fs = require('fs');
 var path = 'courses.json';
 page.onConsoleMessage = function(msg) {
-    console.log('New course: ' + msg);
+    if (msg == '') {
+        console.log('Error: "could not load the course"');
+    } else {
+        console.log('New course: ' + msg);
+    }
 };
 page.open('https://www.bcc.kz/about/kursy-valyut/', function(status) {
     console.log("Status: " + status);
@@ -10,12 +14,16 @@ page.open('https://www.bcc.kz/about/kursy-valyut/', function(status) {
         setTimeout(function () {
             var courses = page.evaluate(function() {
                 var courseObj = { courses: {} };
-                courseObj.courses.usd = $('.bcc_full .s_table_over:nth-child(5) tbody tr:nth-child(3) td:nth-child(3)').text();
+                courseObj.courses.usd = $('.bcc_fulla .s_table_over:nth-child(5) tbody tr:nth-child(3) td:nth-child(3)').text();
                 console.log(courseObj.courses.usd);
                 return courseObj;
             }, 'courses');
-            fs.write(path, JSON.stringify(courses, null, 2), 'w');
-            phantom.exit();
+            if (courses.courses.usd == '') {
+                phantom.exit();
+            } else {
+                fs.write(path, JSON.stringify(courses, null, 2), 'w');
+                phantom.exit();
+            }
         }, 5000);
     } else {
         phantom.exit();
